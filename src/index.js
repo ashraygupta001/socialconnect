be45cc53fa19602cb9d1b05612623dab9d7d71c5
Ashray <ashray79gupta@gmail.com>
2020-11-04 20:12:45 +0530

    added ssr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {hydrate} from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+hydrate(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
